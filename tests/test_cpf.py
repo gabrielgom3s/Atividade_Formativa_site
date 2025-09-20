993a9e2b7b05cd920d5dd2bfb3989b69a0c3e213
gabrielgom3s <144627181+gabrielgom3s@users.noreply.github.com>
2025-09-20 17:01:01 -0300

    Update test_cpf.py

--- tests/test_cpf.py
+++ tests/test_cpf.py
@@ -1,5 +1,6 @@
-import pytest
-import src.main as main
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+import main
 
 def test_cpf_valido():
     """CPF válido conhecido"""
