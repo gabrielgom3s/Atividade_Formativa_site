ca1cbcffe4a1a2a551fb4be080d9f124ea09a2d6
gabrielgom3s <144627181+gabrielgom3s@users.noreply.github.com>
2025-09-20 16:50:01 -0300

    Inclusões test

--- tests/test_cpf.py
+++ tests/test_cpf.py
@@ -1,11 +1,26 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+import pytest
 import main
 
 def test_cpf_valido():
+    """CPF válido conhecido"""
     assert main.validar_cpf("52998224725") == True
 
 def test_cpf_invalido():
+    """CPF inválido conhecido"""
     assert main.validar_cpf("12345678900") == False
+
+def test_cpf_menor_que_11():
+    """CPF menor que 11 dígitos deve retornar 1"""
+    assert main.validar_cpf("12345") == 1
+
+def test_cpf_com_pontos_tracos():
+    """CPF válido com pontos e traço deve ser aceito"""
+    assert main.validar_cpf("529.982.247-25") == True
+
+def test_cpf_repetido():
+    """CPF com todos os dígitos iguais é inválido"""
+    assert main.validar_cpf("11111111111") == False
+
+def test_cpf_invalido_aleatorio():
+    """Outro CPF aleatório inválido"""
+    assert main.validar_cpf("98765432100") == False
